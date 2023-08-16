f04ce3ae0f491762bf40fb10b2b2901936f4ab54
ajmannust41288 <124865655+ajmannust41288@users.noreply.github.com>
2023-08-16 10:36:21 +0500

    tvtech
    professional landing page

--- admin-functions.php
+++ admin-functions.php
@@ -1,15 +1,85 @@
 <?php
 /**
- * Administration Functions
+ * Admin functions - Functions that add some functionality to WordPress admin panel
  *
- * This file is deprecated, use 'wp-admin/includes/admin.php' instead.
- *
- * @deprecated 2.5.0
- * @package WordPress
- * @subpackage Administration
+ * @package Astra
+ * @since 1.0.0
  */
 
-_deprecated_file( basename( __FILE__ ), '2.5.0', 'wp-admin/includes/admin.php' );
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 
-/** WordPress Administration API: Includes all Administration functions. */
-require_once ABSPATH . 'wp-admin/includes/admin.php';
+/**
+ * Register menus
+ */
+if ( ! function_exists( 'astra_register_menu_locations' ) ) {
+
+	/**
+	 * Register menus
+	 *
+	 * @since 1.0.0
+	 */
+	function astra_register_menu_locations() {
+
+		/**
+		 * Primary Menus
+		 */
+		register_nav_menus(
+			array(
+				'primary' => __( 'Primary Menu', 'astra' ),
+			)
+		);
+
+		if ( true === Astra_Builder_Helper::$is_header_footer_builder_active ) {
+
+			/**
+			 * Register the Secondary & Mobile menus.
+			 */
+			register_nav_menus(
+				array(
+					'secondary_menu' => __( 'Secondary Menu', 'astra' ),
+					'mobile_menu'    => __( 'Off-Canvas Menu', 'astra' ),
+				)
+			);
+
+
+			$component_limit = defined( 'ASTRA_EXT_VER' ) ? Astra_Builder_Helper::$component_limit : Astra_Builder_Helper::$num_of_header_menu;
+
+			for ( $index = 3; $index <= $component_limit; $index++ ) {
+
+				if ( ! is_customize_preview() && ! Astra_Builder_Helper::is_component_loaded( 'menu-' . $index ) ) {
+					continue;
+				}
+
+				register_nav_menus(
+					array(
+						'menu_' . $index => __( 'Menu ', 'astra' ) . $index,
+					)
+				);
+			}
+
+			/**
+			 * Register the Account menus.
+			 */
+			register_nav_menus(
+				array(
+					'loggedin_account_menu' => __( 'Logged In Account Menu', 'astra' ),
+				)
+			);
+
+		}
+
+		/**
+		 * Footer Menus
+		 */
+		register_nav_menus(
+			array(
+				'footer_menu' => __( 'Footer Menu', 'astra' ),
+			)
+		);
+
+	}
+}
+
+add_action( 'init', 'astra_register_menu_locations' );
--- widgets.php
+++ widgets.php
@@ -1,35 +1,211 @@
 <?php
 /**
- * Widget administration screen.
+ * Widget and sidebars related functions
  *
- * @package WordPress
- * @subpackage Administration
+ * @package     Astra
+ * @author      Astra
+ * @copyright   Copyright (c) 2020, Astra
+ * @link        https://wpastra.com/
+ * @since       Astra 1.0.0
  */
 
-/** WordPress Administration Bootstrap */
-require_once __DIR__ . '/admin.php';
-
-/** WordPress Administration Widgets API */
-require_once ABSPATH . 'wp-admin/includes/widgets.php';
-
-if ( ! current_user_can( 'edit_theme_options' ) ) {
-	wp_die(
-		'<h1>' . __( 'You need a higher level of permission.' ) . '</h1>' .
-		'<p>' . __( 'Sorry, you are not allowed to edit theme options on this site.' ) . '</p>',
-		403
-	);
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
 
-if ( ! current_theme_supports( 'widgets' ) ) {
-	wp_die( __( 'The theme you are currently using is not widget-aware, meaning that it has no sidebars that you are able to change. For information on making your theme widget-aware, please <a href="https://developer.wordpress.org/themes/functionality/widgets/">follow these instructions</a>.' ) );
-}
+/**
+ * WordPress filter - Widget Tags
+ */
+if ( ! function_exists( 'astra_widget_tag_cloud_args' ) ) :
 
-// Used in the HTML title tag.
-$title       = __( 'Widgets' );
-$parent_file = 'themes.php';
+	/**
+	 * WordPress filter - Widget Tags
+	 *
+	 * @param  array $args Tag arguments.
+	 * @return array       Modified tag arguments.
+	 */
+	function astra_widget_tag_cloud_args( $args = array() ) {
 
-if ( wp_use_widgets_block_editor() ) {
-	require ABSPATH . 'wp-admin/widgets-form-blocks.php';
-} else {
-	require ABSPATH . 'wp-admin/widgets-form.php';
-}
+		$sidebar_link_font_size            = astra_get_option( 'font-size-body' );
+		$sidebar_link_font_size['desktop'] = ( '' != $sidebar_link_font_size['desktop'] ) ? $sidebar_link_font_size['desktop'] : 15;
+
+		$args['smallest'] = intval( $sidebar_link_font_size['desktop'] ) - 2;
+		$args['largest']  = intval( $sidebar_link_font_size['desktop'] ) + 3;
+		$args['unit']     = 'px';
+
+		return apply_filters( 'astra_widget_tag_cloud_args', $args );
+	}
+	add_filter( 'widget_tag_cloud_args', 'astra_widget_tag_cloud_args', 90 );
+
+endif;
+
+/**
+ * WordPress filter - Widget Categories
+ */
+if ( ! function_exists( 'astra_filter_widget_tag_cloud' ) ) :
+
+	/**
+	 * WordPress filter - Widget Categories
+	 *
+	 * @param  array $tags_data Tags data.
+	 * @return array            Modified tags data.
+	 */
+	function astra_filter_widget_tag_cloud( $tags_data ) {
+
+		if ( is_tag() ) {
+			foreach ( $tags_data as $key => $tag ) {
+				if ( get_queried_object_id() === (int) $tags_data[ $key ]['id'] ) {
+					$tags_data[ $key ]['class'] = $tags_data[ $key ]['class'] . ' current-item';
+				}
+			}
+		}
+
+		return apply_filters( 'astra_filter_widget_tag_cloud', $tags_data );
+	}
+	add_filter( 'wp_generate_tag_cloud_data', 'astra_filter_widget_tag_cloud' );
+
+endif;
+
+/**
+ * Register widget area.
+ */
+if ( ! function_exists( 'astra_widgets_init' ) ) :
+
+	/**
+	 * Register widget area.
+	 *
+	 * @see https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+	 */
+	function astra_widgets_init() {
+
+		/**
+		 * Register Main Sidebar
+		 */
+		register_sidebar(
+			apply_filters(
+				'astra_widgets_init',
+				array(
+					'name'          => esc_html__( 'Main Sidebar', 'astra' ),
+					'id'            => 'sidebar-1',
+					'description'   => '',
+					'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+					'after_widget'  => '</aside>',
+					'before_title'  => '<h2 class="widget-title">',
+					'after_title'   => '</h2>',
+				)
+			)
+		);
+
+		/**
+		 * Register Header Widgets area
+		 */
+		register_sidebar(
+			apply_filters(
+				'astra_header_widgets_init',
+				array(
+					'name'          => esc_html__( 'Header', 'astra' ),
+					'id'            => 'header-widget',
+					'description'   => '',
+					'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+					'after_widget'  => '</aside>',
+					'before_title'  => '<h2 class="widget-title">',
+					'after_title'   => '</h2>',
+				)
+			)
+		);
+
+		/**
+		 * Register Footer Bar Widgets area
+		 */
+		register_sidebar(
+			apply_filters(
+				'astra_footer_1_widgets_init',
+				array(
+					'name'          => esc_html__( 'Footer Bar Section 1', 'astra' ),
+					'id'            => 'footer-widget-1',
+					'description'   => '',
+					'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+					'after_widget'  => '</aside>',
+					'before_title'  => '<h2 class="widget-title">',
+					'after_title'   => '</h2>',
+				)
+			)
+		);
+
+		register_sidebar(
+			apply_filters(
+				'astra_footer_2_widgets_init',
+				array(
+					'name'          => esc_html__( 'Footer Bar Section 2', 'astra' ),
+					'id'            => 'footer-widget-2',
+					'description'   => '',
+					'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+					'after_widget'  => '</aside>',
+					'before_title'  => '<h2 class="widget-title">',
+					'after_title'   => '</h2>',
+				)
+			)
+		);
+
+		/**
+		 * Register Footer Widgets area
+		 */
+		register_sidebar(
+			apply_filters(
+				'astra_advanced_footer_widget_1_args',
+				array(
+					'name'          => esc_html__( 'Footer Widget Area 1', 'astra' ),
+					'id'            => 'advanced-footer-widget-1',
+					'before_widget' => '<div id="%1$s" class="widget %2$s">',
+					'after_widget'  => '</div>',
+					'before_title'  => '<h2 class="widget-title">',
+					'after_title'   => '</h2>',
+				)
+			)
+		);
+
+		register_sidebar(
+			apply_filters(
+				'astra_advanced_footer_widget_2_args',
+				array(
+					'name'          => esc_html__( 'Footer Widget Area 2', 'astra' ),
+					'id'            => 'advanced-footer-widget-2',
+					'before_widget' => '<div id="%1$s" class="widget %2$s">',
+					'after_widget'  => '</div>',
+					'before_title'  => '<h2 class="widget-title">',
+					'after_title'   => '</h2>',
+				)
+			)
+		);
+
+		register_sidebar(
+			apply_filters(
+				'astra_advanced_footer_widget_3_args',
+				array(
+					'name'          => esc_html__( 'Footer Widget Area 3', 'astra' ),
+					'id'            => 'advanced-footer-widget-3',
+					'before_widget' => '<div id="%1$s" class="widget %2$s">',
+					'after_widget'  => '</div>',
+					'before_title'  => '<h2 class="widget-title">',
+					'after_title'   => '</h2>',
+				)
+			)
+		);
+
+		register_sidebar(
+			apply_filters(
+				'astra_advanced_footer_widget_4_args',
+				array(
+					'name'          => esc_html__( 'Footer Widget Area 4', 'astra' ),
+					'id'            => 'advanced-footer-widget-4',
+					'before_widget' => '<div id="%1$s" class="widget %2$s">',
+					'after_widget'  => '</div>',
+					'before_title'  => '<h2 class="widget-title">',
+					'after_title'   => '</h2>',
+				)
+			)
+		);
+	}
+	add_action( 'widgets_init', 'astra_widgets_init' );
+
+endif;
